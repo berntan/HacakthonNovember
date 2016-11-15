b48b29960f10f9564d3c09d3d8b2a52238ddaf71
Eirik Grøttum <eirik.grottum@usit.uio.no>
2016-11-15 14:45:45 +0100

    add material icons

--- hackaton/jvm/src/main/scala-2.11/hackaton/Server.scala
+++ hackaton/jvm/src/main/scala-2.11/hackaton/Server.scala
@@ -24,7 +24,8 @@ object Template {
           script(`type` := "text/javascript", src := "/client-fastopt.js"),
           script(`type` := "text/javascript", src := "https://code.jquery.com/jquery-2.1.1.min.js"),
           link(rel := "stylesheet", href := "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css"),
-          script(src := "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js")
+          script(src := "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"),
+          link(href := "https://fonts.googleapis.com/icon?family=Material+Icons", rel := "stylesheet")
         ),
         body(
           script("hackaton.App().main()")
